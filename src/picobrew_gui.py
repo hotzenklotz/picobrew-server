14ab61a6afc0a3f6d53dd216e64f6dcfc0b6d353
Christoph Sterz <christoph.sterz@student.hpi.uni-potsdam.de>
2015-06-28 16:42:02 +0200

    some mocked recipes

--- src/picobrew_gui.py
+++ src/picobrew_gui.py
@@ -2,17 +2,34 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from PyQt5.QtGui import * 
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
 class RecipeAndStatus(QWidget):
-    def __init__(self, parent=None, items=[]):
+    def __init__(self, parent=None, recipies=[]):
         super(RecipeAndStatus, self).__init__(parent)
-        self.recipeView = QListView()
         
-        #layout
+        #ListView
+        self.recipeList = QListView()
+        self.recipeList.setUniformItemSizes(True)
+        self.recipeList.setSelectionRectVisible(True)
+        #ListModel
+        self.recipeModel = QStandardItemModel(self.recipeList)
+        for recipe in recipies:
+            item = QStandardItem()
+            item.setText(recipe)
+            item.setEditable(False)
+            item.setSizeHint(QSize(50,50))
+            self.recipeModel.appendRow(item)
+        self.recipeList.setModel(self.recipeModel)
+
+        #Layout
         mainLayout = QGridLayout()
-        mainLayout.addWidget(self.recipeView)
+        self.updateButton = QPushButton("Update Recipes from Picobrew")
+
+        mainLayout.addWidget(self.recipeList)
+        mainLayout.addWidget(self.updateButton)
         self.setLayout(mainLayout)
 
 
@@ -29,16 +46,19 @@ class Form(QMainWindow):
         #Widgets
         #SplitView of InfoGraphs and Recipes
         self.splitter = QSplitter(Qt.Vertical)
+        self.splitter.setStyleSheet("Qsplitter{margin-top:20px;}")
 
         #Tabs for Possible Action Groups (i.e. brewing and Cleaning)
         self.tabWidget = QTabWidget()
-        self.brewingRecipes = RecipeAndStatus()
+        self.brewingRecipes = RecipeAndStatus(recipies=["Independence Pale Ale", "Eier Bräu"])
+
         self.tabWidget.addTab(self.brewingRecipes, "Brewing")
-        self.cleaningRecipes = RecipeAndStatus()
+        self.cleaningRecipes = RecipeAndStatus(recipies=["Cleaning v1", "Rinse v1", "Pump"])
         self.tabWidget.addTab(self.cleaningRecipes, "Cleaning")
         #################split###################
         #Graphs
         self.graphs = QLabel("Graphs")
+        self.graphs.setMinimumSize(200,200)
         self.graphs.setStyleSheet("QLabel {background-color : grey; color : white; }")
 
         #put both in split view
@@ -50,15 +70,19 @@ class Form(QMainWindow):
         self.statusBar().showMessage("Waiting for Picobrew to connect…")
 
     #MenuBarActions
-    def openRecipes(self): 
-        pass
-
+    def openRecipes(self):
+        fileDialog=QFileDialog( self )
+        fileDialog.setWindowTitle("Choose Recipes from Disk" )
+        fileDialog.setViewMode( QFileDialog.Detail )
+        fileDialog.setNameFilters( [self.tr('Brew Files (*.brew)'), self.tr('Brew XML (*.xml)'), self.tr('All Files (*)')] )
+        fileDialog.setDefaultSuffix( '.brew' )
+        if fileDialog.exec_() :
+            print fileDialog
 
 if __name__ == '__main__':
     import sys
  
     app = QApplication(sys.argv)
- 
     screen = Form()
     screen.show()
  
