0831ef923289a01c45acc33fee9baf9275230be0
Tom Herold <tom.herold@scalableminds.com>
2015-08-16 21:35:20 +0200

    implemented route parsing for PicoBrew API

--- blueprints/picobrew_API.py
+++ blueprints/picobrew_API.py
@@ -1,9 +1,76 @@
 from flask import *
-from werkzeug import secure_filename
+from webargs import Arg
+from webargs.flaskparser import use_kwargs, FlaskParser
+
+
+SYSTEM_USER = "00000000000000000000000000000000"
+arg_parser = FlaskParser()
 
 picobrew_api = Blueprint("picobrew_api", __name__)
 
+# Unfortunately the PicoBrew API ony consist of three overloaded routes.
+
+# ----------- RECIPES -----------
 @picobrew_api.route("/API/SyncUser")
-def initialize():
-    print request.args
+@use_kwargs({"user": Arg(str), "machine": Arg(str)})
+def parse_recipe_request(user, machine):
+
+    if user == SYSTEM_USER:
+        return getCleaningRecipes()
+    else:
+        if machine:
+            return getAllRecipes()
+
+    # default response
     return "\r\n##"
+
+
+def getCleaningRecipes():
+    return "cleaning"
+
+
+def getAllRecipes():
+    return "all recipes"
+
+
+# ----------- SESSION LOGGING -----------
+@picobrew_api.route("/API/LogSession")
+@use_kwargs({"session": Arg(str), "recipe": Arg(str)})
+def parse_session_request(session, recipe):
+
+    if recipe:
+        args = arg_parser.parse({"user": Arg(str), "firm": Arg(str), "machine": Arg(str)})
+        return create_new_session(recipe, args)
+
+    if session:
+        args = arg_parser.parse({"data": Arg(str), "state": Arg(str), "step": Arg(int), "code": Arg(int)})
+        return log_to_session(session, args)
+
+    # default fallthrough
+    abort()
+
+
+def create_new_session(recipeId, args):
+    raise NotImplementedError()
+    # return sessionID
+
+
+def log_to_session(sessionId, args):
+    raise NotImplementedError()
+    # return ""
+
+# ----------- SESSION RECOVERY -----------
+@picobrew_api.route("/API/recoversession")
+@use_kwargs({"session": Arg(str), "code": Arg(int)})
+def parse_session_recovery_request(session, code):
+
+    if code == 0:
+        raise NotImplementedError()
+        # return recipe
+
+    elif code == 1:
+        raise NotImplementedError()
+        # return machine params
+
+    # default fallthrough
+    abort()
